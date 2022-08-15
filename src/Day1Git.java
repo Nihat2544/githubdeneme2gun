6cdcb4fd8565eee224fb17d5565efaa23a6ab76b
Nihat Kızılkula <nht.4425@gmail.com>
2022-08-15 22:08:35 +0200

    B2

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,14 +1,14 @@
 public class Day1Git {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
+
         System.out.println("Hell Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
         System.out.println("Hello Git 3. Deneme");
         System.out.println("Hello Git 4. Deneme");
-=======
+
         System.out.println("Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
->>>>>>> Nihat
+
     }
 }
--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -18,7 +18,6 @@ public class Day2GitGithub {
 
      */
     public static void main(String[] args) {
-
         System.out.println("Branch deneme 1");
         System.out.println("Master 1. deneme");
     }
